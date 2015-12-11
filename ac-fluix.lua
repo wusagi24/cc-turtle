7cae695a0b7499e4112ebf3574fa047f604b4dd9
nishi.w <wusagi@example.com>
2015-12-12 03:43:42 +0900

    add simple ver.

--- ac-fluix.lua
+++ ac-fluix.lua
@@ -1,29 +1,50 @@
 -------------------------------------------------
 -- AE2 auto craft 'fluix crystal' 自動生成用プログラム
--- (一度に128個まで注文可能)
 --
 -- creater 'wusagi24'
 -------------------------------------------------
 
+local material_1_slot = 1
+local material_2_slot = 2
+local material_3_slot = 3
+
+local product_slot = 16
+
+while true do
 
 -- インベントリにアイテムが入るのを待つ
-
+	local evt = os.pullEvent("turtle_inventory")
 -- インベントリにアイテムが入る
--- 最大1スタック分の搬入が終わるまでディレイをかける
+	print("change inventory")
+	print()
 
--- 素材スロットに該当アイテム以外が入ったら正面に送り出して待機状態に戻る
--- 素材スロットに該当アイテムセットが入ったら以降実行
+-- インベントリに素材がある程度搬入されるまで待つ
+	-- sleep(3)
 
--- 素材スロットのアイテムを32セット真下にドロップする　[*]
+	repeat
 
--- 一定時間待機する
+--		素材スロットのアイテムを32セット真下にドロップする　[*]
+		turtle.select(material_1_slot)
+		turtle.dropDown(1)
+		turtle.select(material_2_slot)
+		turtle.dropDown(1)
+		turtle.select(material_3_slot)
+		turtle.dropDown(1)
 
--- 真下のアイテムを完成品スロットに拾う
+--		一定時間待機する
+		sleep(4)
 
--- 完成品スロットのアイテムを正面に送り出す
+--		完成品スロットに移動
+		turtle.select(product_slot)
 
--- 素材スロットにまだアイテムセットがあれば[*] に戻る
+--		真下のアイテムを完成品スロットに拾う
+		while turtle.suckDown() do
+--			完成品スロットのアイテムを正面に送り出す
+			turtle.drop()
+		end
 
--- すべてのスロットのアイテムを正面に送り出す
+--	素材スロットにまだアイテムセットがあれば[*] に戻る
+	until turtle.getItemCount(material_1_slot) <= 0
 
 -- 待機状態に戻る
+end
